d8058ad093293b85f474893c1335a9b75382e4d3
Nick Pafundi <napafundi@gmail.com>
2019-08-12 20:47:29 -0400

    complete rework of page, more simple design

--- app/static/home.js
+++ app/static/home.js
@@ -1,13 +1,26 @@
-//display modal for image clicked
-function modalDisplay(obj) {
-    var imgId = obj.id;
-    var modalId = imgId + "_modal";
-    var modalImgId = modalId + "_img";
-    var captionId = obj.id + "_caption";
-    var modal = document.getElementById(modalId);
-    var modalImg = document.getElementById(modalImgId);
-    var caption = document.getElementById(captionId);
-    modal.style.display = "block";
-    modalImg.src = obj.src;
-    caption.innerHTML = obj.alt;
-}
+//Wait to run your initialization code until the DOM is fully loaded. This is needed
+// when wanting to access elements that are later in the HTML than the <script>.
+document.addEventListener('DOMContentLoaded', function(event) {;
+    var modalBtn = document.getElementById("contactBtn");
+    var modalView = document.getElementById("contactModal");
+    var modalClose = document.getElementById("modalClose");
+    var body = document.getElementById("mainContent");
+    // display contactModal on link click
+    modalBtn.onclick = function() {
+        modalView.style.display = "block";
+        body.className = ("mainContent blur-filter");
+    }
+
+    modalClose.onclick = function() {
+        modalView.style.display = "none";
+        body.className = ("mainContent");
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modalView) {
+            modalView.style.display = "none";
+            body.className = ("mainContent");
+        }
+    }
+});
+
